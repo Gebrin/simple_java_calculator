8e467aa1cebcecb60bd050a1c0ae67b94bca33f4
Gebrin <gebrinbiju123@gmail.com>
2023-07-15 15:56:16 +0530

    imported package and Created the instance

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,24 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+class Calculator implements ActionListener {
+    JFrame frame;
+    JTextField textfield;
+    JButton[] numberButtons = new JButton[10];
+    JButton[] functionButtons = new JButton[8];
+    JButton addButton, subButton, mulButton, divButton;
+    JButton decButton, equButton, delButton, clrButton;
+    JPanel panel;
 
-public class Main {
+    Calculator() {
+        // constructor code
+    }
+
     public static void main(String[] args) {
-        // Press Opt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Calculator calc = new Calculator();
+    }
 
-        // Press Ctrl+R or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Ctrl+D to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Cmd+F8.
-            System.out.println("i = " + i);
-        }
+    public void actionPerformed(ActionEvent e) {
+        // implementation code for handling the action event
     }
 }
